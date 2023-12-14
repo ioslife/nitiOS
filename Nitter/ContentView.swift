b542c94d0569abae573d23ee05811a073207c4ed
iOSLife <developer@ioslife.dev>
2023-12-14 09:27:24 -0500

    Initial Commit

---
+++ Nitter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nitter
+//
+//  Created by Bronson Lane on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nitter/NitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NitterApp.swift
+//  Nitter
+//
+//  Created by Bronson Lane on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
